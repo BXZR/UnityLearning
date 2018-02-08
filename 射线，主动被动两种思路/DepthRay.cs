2265d989a60de2c56fe36414705e82ff20ce08e1
BXZR <405181821@qq.com>
2018-02-08 17:35:43 +0800

    关于射线
    增加关于射线的主被动的小思路

---
+++ 射线，主动被动两种思路/DepthRay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepthRay : MonoBehaviour {
+
+	//方法1，直接用射线碰撞来做
+	//这个是用玩家操纵（主动）
+	void rayPointPosition(Vector3 mousePosition)
+	{
+		Ray theRay = Camera.main.ScreenPointToRay (mousePosition);
+		RaycastHit theHit;
+		if (Physics.Raycast (theRay, out theHit)) {
+			print( "Destination is "+ theHit.point);	
+		}
+	}
+
+	//方法2，用逆透视变换来做
+	//但是其实就是用相似来做的
+	//折耳根是物体Plane感知射线（被动）
+	void rayPointPositionWithDepth(Vector3 mousePosition)
+	{
+		Ray theRay = Camera.main.ScreenPointToRay (mousePosition);
+		float depth = 0;
+		if (this.GetComponent<Plane> ().Raycast (theRay, out depth)) 
+		{
+			print( "Destination is "+ theRay.origin + theRay.direction * depth);	
+		}
+
+	}
+
+	void Update () {
+		
+	}
+}
