087b613843b0b43d925f7d39ff6414f92b1a3e93
BXZR <405181821@qq.com>
2018-04-08 22:29:55 +0800

    优化总结，List使用，细节
    一份看上去还是很细节靠谱的优化总结
    List的New和Clear的效率
    加减乘除的效率对比

---
+++ List Clear New 对比/mousedd.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousedd : MonoBehaviour {
+
+	//对比List  Clear 和 New  
+
+	List<int> A = new List<int> ();
+	void CL()
+	{
+		for (int j = 0; j < 10000; j++) 
+		{
+			for (int i = 0; i < 100; i++)
+				A.Add (i);
+			A.Clear ();
+		}
+	}
+	void NE()
+	{
+		for (int j = 0; j < 10000; j++) 
+		{
+			for (int i = 0; i < 100; i++)
+				A.Add (i);
+			A = new List<int> ();
+		}
+	}
+
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Q))
+			CL ();
+		if (Input.GetKeyDown (KeyCode.W))
+			NE ();
+	}
+}
