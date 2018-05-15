1a1ed8426ffa6e183d0430a72f331f3b913ad2c1
BXZR <405181821@qq.com>
2018-05-15 09:09:51 +0800

    原形范围内鼠标拖动
    原形范围内鼠标拖动
    山寨愤怒小鸟的一个小功能

---
+++ 原型范围鼠标拖动/circleMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circleMove : MonoBehaviour {
+
+	public Transform theCenter;
+	public float R = 3f;
+
+	void move()
+	{
+		Vector3 Mouse3D = Input.mousePosition;
+		Mouse3D.z = theCenter.transform.position.z;
+		Mouse3D = Camera.main.ScreenToWorldPoint (Mouse3D);
+		Vector3 detla = Mouse3D - theCenter.transform.position;
+
+		if (detla.magnitude > R) 
+		{
+			detla = detla.normalized * R;
+		}
+		this.transform.position = theCenter.position + detla;
+	}
+
+	void Update ()
+	{
+		move ();
+	}
+}
