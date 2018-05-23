a29320ffa36b81e3c11f204558b66baf94947ae1
BXZR <405181821@qq.com>
2018-05-23 09:30:31 +0800

    鼠标换图
    鼠标换图
    顺带将图片设定为鼠标图的设定
    实际上应该都是继承Texture2D的做法

---
+++ 代码变换鼠标图/cursor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cursor : MonoBehaviour {
+	public Texture2D [] Cursors;
+
+	//0 普通的
+	//1 怪物
+	//2 介绍
+	//3 寻找目标
+
+	void Start () 
+	{
+		setCursor(0);
+	}
+	void Update () {
+ 
+
+	}
+	public void setCursor(int index)
+	{
+		Cursor .SetCursor (Cursors[index],new Vector2 (0,0),CursorMode.Auto);
+	}
+ 
+ 
+}
