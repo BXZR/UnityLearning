5639da4c19a2ff9cf6e719cbef18a6123145e73b
BXZR <405181821@qq.com>
2018-05-31 22:42:33 +0800

    动画状态机脚本
    动画状态机脚本
    看上去挺实用的回调

---
+++ 动画状态机上的额外脚本/GMDDS.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMDDS :StateMachineBehaviour {
+
+	public override void OnStateEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+	{
+		base.OnStateEnter (animator, stateInfo, layerIndex);
+		Debug.Log ("GFF");
+	}
+}
