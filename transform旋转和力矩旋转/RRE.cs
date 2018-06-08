40859269a1ccd3f5b60ebb4b54ea9eafc908c9f3
BXZR <405181821@qq.com>
2018-06-08 11:34:17 +0800

    力矩和transform旋转的区别
    力矩和transform旋转的区别

---
+++ transform旋转和力矩旋转/RRE.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class RRE : MonoBehaviour {
+
+	public bool  force = true;
+	public float speed = 20f;
+	Rigidbody R;
+	void Start () {
+		R = this.GetComponent <Rigidbody> ();
+		R.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(!force)
+			this.transform.Rotate (Vector3.up * speed*Time .deltaTime  , Space.Self);
+		if(force)
+			R.AddTorque (Vector3.up * speed *Time .deltaTime);
+	}
+	 
+}
+    
