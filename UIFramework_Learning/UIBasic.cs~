dc122f814d379c8e9c40257b56ac2496418f1631
BXZR <405181821@qq.com>
2018-11-15 10:14:10 +0800

    一个很简单的UI框架
    一个很简单的UI框架，看看以后能不能好好打磨打磨

---
+++ UIFramework_Learning/UIBasic.cs~
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBasic : MonoBehaviour {
+
+	//所有UICanvas的基类
+
+}
---
+++ UIFramework_Learning/UIController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+	private static UIController theUIcontroller;
+
+	public static UIController GetInstance()
+	{
+		return theUIcontroller;
+	}
+
+	private Dictionary <string , GameObject> UIBook = new Dictionary<string, GameObject>();
+
+	public  void ShowUI <T> () where T : UIBasic
+	{
+		string UIName = typeof(T).ToString ();
+		GameObject theUI;
+		if (!UIBook.TryGetValue (UIName, out theUI))
+		{
+			theUI = (GameObject)Resources.Load ("UI/" + UIName);
+			theUI = Instantiate (theUI);
+			theUI.name = UIName;
+			UIBook.Add (UIName, theUI);
+		}
+		if(theUI)
+		{
+			theUI.SetActive (true);
+		}
+	}
+
+	public  void CloseUI<T> () where T: UIBasic
+	{
+		
+		string UIName = typeof(T).ToString ();
+		GameObject theUI;
+		if (!UIBook.TryGetValue (UIName, out theUI))
+		{
+			theUI = (GameObject)Resources.Load ("UI/" + UIName);
+			theUI = Instantiate (theUI);
+			theUI.name = UIName;
+			UIBook.Add (UIName, theUI);
+		}
+		if(theUI)
+		{
+			theUI.SetActive (false);
+		}
+	}
+
+
+	void Start()
+	{
+		theUIcontroller = this;
+	}
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.A))
+			ShowUI<PlayerActCanvas> ();
+		if (Input.GetKeyDown (KeyCode.S))
+			CloseUI<PlayerActCanvas> ();
+	}
+}
