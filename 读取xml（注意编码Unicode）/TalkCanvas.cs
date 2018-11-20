fbebaf9f2d4867d05dc0a52c6b851febade7b4ff
BXZR <405181821@qq.com>
2018-11-20 21:34:16 +0800

    XML读取套路
    XML读取套路
    顺便追忆一下皮安娜的故事

---
+++ 读取xml（注意编码Unicode）/TalkCanvas.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml;
+
+public class TalkCanvas : UIBasic {
+
+	//显示对话
+	XmlDocument xml = new XmlDocument();
+	XmlNodeList theXmlList;
+
+
+
+	void OnEnable()
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+
+	public override void OnShow (string value = "")
+	{
+		TextAsset textAsset = (TextAsset)Resources.Load ("XML/" + value);
+		xml.LoadXml (textAsset.text);
+		theXmlList = xml.SelectNodes ("Root/Dialog");
+		foreach (XmlNode node in theXmlList) 
+		{
+			print (node.SelectSingleNode("Name").InnerText);
+			print (node.SelectSingleNode("Picture").InnerText);
+			print (node.SelectSingleNode("Information").InnerText);
+		}
+	} 
+}
