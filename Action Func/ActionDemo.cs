0aa449ff8ab6e1cf5474a4d1209cdc6c3be6aa40
fumeng <405181821@qq.com>
2019-06-14 11:22:54 +0800

    开始收集插件库

---
+++ Action Func/ActionDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionDemo : MonoBehaviour
+{
+    //无返回类型用action
+    Action<string> theAction;
+    //有返回类型用func
+    //前面是参数类型，后面是返回值类型
+    Func<int, string> theFunc;
+
+    private void Start()
+    {
+        theAction += M1;
+        theAction += M2;
+
+        theFunc += M3;
+        theFunc += M4;
+
+        //最终被调用的是M1M2M4
+
+    }
+
+
+    private void M1(string A)
+    {
+        print(A);
+    }
+    private void M2(string A)
+    {
+        print(A+"----"+A);
+    }
+
+
+    private string M3(int a)
+    {
+        return a + "---";
+    }
+    private string M4(int a)
+    {
+        return a + 998 + "yus";
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            theAction("suck");
+            print(theFunc(9998));
+        }
+    }
+}
---
+++ processBar(Editor进度条)/ProcessBarDemo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ProcessBarDemo : EditorWindow
+{
+
+    [MenuItem("Tools/进度条")]
+    private static void ShowSlider()
+    {
+        int index = 0;
+        int total = 500;
+        EditorApplication.update = delegate ()
+        {
+            bool isCancle = EditorUtility.DisplayCancelableProgressBar("执行进度条示例中...", "正在走进度条", (float)index / total);
+            ++index;
+            if (isCancle || index >= total)
+            {
+                EditorUtility.ClearProgressBar();
+                EditorApplication.update = null;
+            }
+        };
+    }
+}
