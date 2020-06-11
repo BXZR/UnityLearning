8a0aa74190bafe9e91c5ad8c9d5821a7ea8ec886
fumeng <405181821@qq.com>
2020-06-11 14:14:08 +0800

    添加组件的时候顺带进行检查

---
+++ 添加组件的时候顺带进行检查.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR//在编译器环境下运行
+using UnityEditor;
+#endif
+
+using UnityEngine;
+
+public class cccc : MonoBehaviour
+{
+#if UNITY_EDITOR//在编译器环境下运行
+    void OnValidate()
+
+    {
+
+        Debug.Log("there is a change");
+
+    }
+
+    void Reset()
+
+    {
+
+         if(this.gameObject.GetComponents<cccc>().Length > 1)
+        {
+            EditorUtility.DisplayDialog("error", "There is already a cccc script", "known");
+            DestroyImmediate(this);
+        }
+
+    }
+#endif
+}
